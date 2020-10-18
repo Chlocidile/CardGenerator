045b83b513aa4f84c57caacf70e050125434a75a
Chloe Robertson <chlo0chlo0ee@gmail.com>
2020-10-18 14:28:11 +0100

    add program that outputs ace of spades

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CardGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var card = new Card(Suit.Spades, 1);
+            Console.WriteLine(card);
+        }
+    }
+
+    public enum Suit
+    {
+        Hearts = 0,
+        Diamonds = 1,
+        Spades = 2,
+        Clubs = 3,
+    }
+    
+    public class Card
+    {
+        public Suit Suit { get; }
+        public int Value { get; }
+        
+        public Card(Suit suit, int value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+    }
+}
